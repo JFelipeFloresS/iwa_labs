8715a8673a26bb8b60354d866199c4819e43e7d5
Felipe Flores <62604435+JFelipeFloresS@users.noreply.github.com>
2022-04-01 09:48:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const http = require("http"),
     logger = require("morgan"),
     express = require("express"),
     itemCtrl = require("./item-controller"),
-    bodyParser = require("body-parser");
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose");
 
 let app = express();
 let port = 8000;
@@ -10,6 +11,16 @@ let port = 8000;
 app.use(require('./routes'));
 app.use(bodyParser.json());
 app.use(logger("tiny"));
+
+mongoose.connect('mongodb://localhost/test');
+
+mongoose.connection.on('error', (err) => {
+    console.log('Mongodb error ', err, '.');
+    process.exit();
+});
+mongoose.connection.on('connected', () => {
+    console.log('MongoDB successfully connected.');
+});
 
 app.listen(port, function(err) {
     console.log("Listening on port: " + port);
